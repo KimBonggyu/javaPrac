7cb60dfaf2bde318a5efce2e5f8c9dba8838c6c0
Kimbonggyu <kbgkbg1999@gmail.com>
2021-07-24 15:56:05 +0900

    - Modify dependency injection with AppConfig - Previously, the interface relied on implementation classes, but from outside (AppConfig), select an implementation class to inject dependencies

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,20 +1,25 @@
 package hello.core.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.order.Order;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderServiceTest {
 	
-	MemberService memberService = new MemberServiceImpl();
-	OrderService orderService = new OrderServiceImpl();
+	MemberService memberService;
+	OrderService orderService;
+	
+	@BeforeEach
+	public void beforEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+		orderService = appConfig.orderService();
+	}
 	
 	@Test
 	void createOrder() {
